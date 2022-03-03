6cd79b2106307db8ca193b4c76ba392e8e6d2c63
nprodas <nprodas@cougarnet.uh.edu>
2022-03-03 11:48:41 -0600

    llama bot, csv

---
+++ Llama_Check_BOT.py
@@ -0,0 +1,22 @@
+import csv
+from discord.ext import commands
+import token
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.command()
+async def llama(ctx, llama_number):
+    with open('Llama_master.csv', 'r') as llamaFile:
+        reader = csv.reader(llamaFile)
+
+        for row in reader:
+            if row[0] == llama_number:
+                image = row[2]
+                rarity = row[3]
+
+    response = ':llama: **Llama #{} is {}!** :llama:\n'.format(llama_number, rarity)
+    await ctx.channel.send(response)
+    await ctx.channel.send(image)
+
+
+bot.run(token.token)
